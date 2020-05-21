abf81113c98675d431bdb72e957a13bc622ab0bb
DinukaJ <dinukasandaruwan.ds@gmail.com>
2020-05-21 15:37:44 +0530

    Update bookCost.scala

--- Assignment 1/BookCost/bookCost.scala
+++ Assignment 1/BookCost/bookCost.scala
@@ -1,7 +1,14 @@
 object bookCost extends App{
     def booksFullAmount(bCount:Integer):Double=bCount*24.95;
     def discount(price:Double):Double=price*0.4;
-    def shipping(bCount:Integer):Double=3+(bCount-50)*0.75;
+    def shipping(bCount:Integer):Double={
+        var shipping:Double=3;
+        if(bCount>50)
+        {
+            shipping=shipping+(bCount-50)*0.75;
+        }
+        shipping;
+    }
 
     printf("%.2f",booksFullAmount(60)-discount(booksFullAmount(60))+shipping(60));
 }
