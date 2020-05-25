5fb938f7c2f5ec8acd9c7238b21cce03b5fff666
DinukaJ <dinukasandaruwan.ds@gmail.com>
2020-05-25 16:58:07 +0530

    Addition of Even Numbers Less than n

---
+++ Assignment 3/evenAddition/evenAddition.scala
@@ -0,0 +1,11 @@
+object evenAddition extends App{
+    def isEven(n:Int):Boolean = if(n%2==0) true else false
+
+    def getEvenAddition(n:Int, m:Int=1):Int = m match {
+        case m if(m==n) => return 0
+        case m if(isEven(m)) => return m+getEvenAddition(n,m+1)
+        case m => getEvenAddition(n,m+1)
+    }
+
+    println(getEvenAddition(10))
+}
