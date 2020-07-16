bfad856f5bcb4ff07b61a4620a381cca336db760
DinukaJ <dinukasandaruwan.ds@gmail.com>
2020-07-16 22:18:40 +0530

    Bank Account

--- Assignment - Functions and Data/Rational Numbers/RationalNums.scala
+++ Assignment - Functions and Data/Rational Numbers/RationalNums.scala
@@ -15,5 +15,5 @@ object RationalNums extends App{
     val x= new Rational(3,4)
     val y= new Rational(5,8)
     val z= new Rational(2,7)
-    println((x-y-z).toString());
+    println((x-y-z).toString())
 }
