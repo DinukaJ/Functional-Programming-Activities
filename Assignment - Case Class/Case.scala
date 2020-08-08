194860337244ff4291f14332bba718df7b07ad90
DinukaJ <dinukasandaruwan.ds@gmail.com>
2020-08-08 22:12:43 +0530

    Update Case.scala

--- Assignment - Case Class/Case.scala
+++ Assignment - Case Class/Case.scala
@@ -7,7 +7,7 @@ case class Point(x:Int, y:Int)
 
      def move(dx:Int, dy:Int)=Point(this.a+dx, this.b+dy)
 
-     def distance(a:Point, b:Point)=((a.x-b.x),(a.y-b.y))
+     def distance(b:Point)=((this.x-b.x).abs,(this.y-b.y).abs)
 
      def invert=Point(this.y, this.x)
 }
@@ -15,15 +15,18 @@ object Case extends App{
     val p1 = Point(1,2);
     val p2 = Point(4,5);
 
-    println("Point After Defining:- ");
+    println("Point After Defining:- ")
     println(p1);
     println(p2);
 
-    println("\nAdd Two Points:- ");
+    println("\nAdd Two Points:- ")
     println(p1+p2);
 
-    println("\nMove P1 by dx=5 & dy=8 Distance:- ");
+    println("\nMove P1 by dx=5 & dy=8 Distance:- ")
     println(p1.move(5,8))
+
+    println("\nDistance Between P1 and P2:- ")
+    println(p1.distance(p2))
 
     println("\nInvert P2:- ")
     println(p2.invert)
